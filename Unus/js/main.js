f96bad3631d04883dd7ab0508dc7c719335d75e8
EthanHavlik <pixelationator@gmail.com>
2025-02-16 18:24:01 -0700

    Delete Unus directory

--- Unus/js/main.js
+++
@@ -1,5 +0,0 @@
-//version 1 - 1/31/25:  created file
-//version 1.1 - 2/2/25:  added "under construction" alert
-"use strict";
-
-alert("This website is under development.  Please check back at a later date.");
